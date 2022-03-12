f761590cddf0d3be0a6441471bab1f7ef18ccb9d
Vladislav Todorov <it.vtodorov@gmail.com>
2022-03-12 20:56:15 +0200

    Move Decodable logic to a new private item struct to decouple the Feed Feature module from API implementation details

--- EssentialFeed/FeedFeature/FeedItem.swift
+++ EssentialFeed/FeedFeature/FeedItem.swift
@@ -26,14 +26,3 @@ public struct FeedItem: Equatable {
 }
 
 
-extension FeedItem: Decodable {
-    // To match the keys of json structure creating matching coding keys
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case description
-        case location
-        case imageURL = "image"
-    }
-}
-
-
--- EssentialFeed/FeedLoader/RemoteFeedLoader.swift
+++ EssentialFeed/FeedLoader/RemoteFeedLoader.swift
@@ -40,7 +40,8 @@ public final class RemoteFeedLoader {
                 if response.statusCode == 200,
                    let root = try? JSONDecoder().decode(Root.self,
                                                         from: data) {
-                    completion(.success(root.items))
+                    completion(.success(root.items.map
+                                        { $0.item }))
                 } else {
                     completion(.failure(.invalidData))
                     
@@ -55,8 +56,26 @@ public final class RemoteFeedLoader {
 // Because an array inside of item kpath
 // Creating a container for Feeditems received as json objects to decode them later
 private struct Root: Decodable {
-    let items: [FeedItem]
+    let items: [Item]
 }
+
+// Constructor that receives items and maps them into FeedItem
+// The API representation context to hide the knowledge of API from FeedITem
+private struct Item: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+    
+    var item: FeedItem {
+        return FeedItem(
+            id: id,
+            description: description,
+            location: location,
+            imageURL: image)
+    }
+}
+
 
 
 //class HTTPClient {
