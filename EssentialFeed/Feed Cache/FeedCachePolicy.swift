27ac788d7577f37bba8a374a7454203940e41602
Vladislav Todorov <it.vtodorov@gmail.com>
2022-04-05 21:21:10 +0300

    Move 'FeedCachePolicy  to a separate file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,29 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Fenominall on 4/5/22.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    
+    // MARK: - Properties
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    // MARK: - Lifecycle
+    private init() {}
+    
+    // MARK: - Helpers
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day,
+                                              value: maxCacheAgeInDays,
+                                              to: timestamp) else { return false }
+        return date < maxCacheAge
+    }
+}
