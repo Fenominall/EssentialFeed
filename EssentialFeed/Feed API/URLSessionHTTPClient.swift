3311d9458c590b131a8bbe6c55a9222163c3a6af
Vladislav Todorov <it.vtodorov@gmail.com>
2022-05-14 10:06:54 +0300

    Tried to add URLSessionHTTPUploader

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -34,3 +34,30 @@ public final class URLSessionHTTPClient: HTTPClient {
         }.resume()
     }
 }
+
+
+public final class URLSessionHTTPUploader {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func post(to url: URL, withData uploadData: Data, completion: @escaping (HTTPClientResult) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        session.uploadTask(with: request, from: uploadData) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            guard let response = response as? HTTPURLResponse,
+            (200...299).contains(response.statusCode) else {
+                print("DEBUG: SERVER ERROR IN 'URLSessionHTTPUploader'")
+                return
+            }
+
+        }
+    }
+    
+}
